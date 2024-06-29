f339720aab19de319690c857914e7b97e9ebe104
Anthony Gbobo <psalmseins@gmail.com>
2024-06-28 18:33:11 -0700

    Promise

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+//Await  Async
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploadUser = async () => {
+  try {
+    return { photo: await uploadPhoto(), user: await createUser() };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+};
+
+export default asyncUploadUser;
---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+//Handle multiple successful promises
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([body, fullName]) => console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`))
+    .catch(() => console.log('Signup system offline'));
+}
