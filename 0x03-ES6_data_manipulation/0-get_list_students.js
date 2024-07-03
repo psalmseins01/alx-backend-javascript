0b8a2fe2a2e34ea74ef09864dd482c49b273ba37
Anthony Gbobo <psalmseins@gmail.com>
2024-07-02 20:14:05 -0700

    Function named  that returns an array of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,19 @@
-// Fuction that  returns an array of objects
 const getListStudents = () => [
-	{
-		id: 1,
-		firstName: 'Guillaume',
-		location: 'San Francisco',
-	},
-	{
-		id: 2,
-		firstName: 'James',
-		location: 'Columbia',
-	},
-	{
-		id: 5,
-		firstName: 'Serena',
-		location: 'San Francisco',
-	},
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
 ];
 
 export default getListStudents;
