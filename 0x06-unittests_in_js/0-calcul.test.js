7013b2cbf7fe5fa6cd84e75a040d73a9a177b793
Anthony Gbobo <psalmseins@gmail.com>
2024-08-22 20:00:36 -0700

    Task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('floating point whole numbers', () => {
