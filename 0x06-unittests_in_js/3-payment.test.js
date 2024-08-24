0ea50819ca21f6540b67e4d6dface28a8f449021
Anthony Gbobo <psalmseins@gmail.com>
2024-08-23 19:26:14 -0700

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,23 @@
+const mocha = require('mocha');
+const { expect, assert } = require('chai');
 const sinon = require('sinon');
-const Utils = require('./utils');
-const { expect } = require('chai');
+
+const utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
+const { spy } = require('sinon');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+  it('should call calculateNumber', () => {
+    const calcNumSpy = sinon.spy(utils, 'calculateNumber');
+    const consoleSpy = sinon.spy(console, 'log');
 
-    sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+    const apiRequestRes = sendPaymentRequestToApi(100, 20);
+
+    expect(calcNumSpy.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
+    expect(consoleSpy.calledWithExactly('The total is: 120')).to.equal(true);
+    expect(utils.calculateNumber('SUM', 100, 20)).to.equal(apiRequestRes);
+
+    calcNumSpy.restore();
+    consoleSpy.restore();
   });
 });
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,16 +1,33 @@
 const Utils = {
-  calculateNumber(type, a, b) {
-    if (type === 'SUM') {
-      return Math.round(a) + Math.round(b);
-    }
-    if (type === 'SUBTRACT') {
-      return Math.round(a) - Math.round(b);
-    }
-    if (type === 'DIVIDE') {
-      return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
-    }
-    return 0;
+  isNegZero(n) {
+    const num = Number(n);
+    return num === 0 && 1 / num === -Infinity;
   },
-};
+  calculateNumber(type, a, b = 0) {
+    let aNum = Number(a);
+    let bNum = Number(b);
 
-module.exports = Utils;
+    if (Number.isNaN(aNum) || Number.isNaN(bNum))
+      throw TypeError('Parameters must be numbers or able to coerce to number');
+
+    aNum = Math.round(aNum);
+    bNum = Math.round(bNum);
+
+    let quotient;
+
+    switch (type) {
+      case 'SUM':
+        return aNum + bNum;
+      case 'SUBTRACT':
+        return aNum - bNum;
+      case 'DIVIDE':
+        if (bNum === 0) return 'ERROR';
+        quotient = aNum / bNum;
+        return this.isNegZero(quotient) ? 0 : quotient;
+      default:
+        throw Error(
+          'Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".'
+        );
+    }
+  }
+};
