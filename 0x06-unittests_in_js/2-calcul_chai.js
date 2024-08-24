69b9cada79ff03adac22a45e7c27aead24abd025
Anthony Gbobo <psalmseins@gmail.com>
2024-08-23 18:41:25 -0700

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,14 +1,19 @@
-const calculateNumber = (type, a, b) => {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  }
+function calculateNumber(type, a, b) {
+  const m1 = Math.round(a);
+  const m2 = Math.round(b);
+
   if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
+    return m1 - m2;
   }
+
   if (type === 'DIVIDE') {
-    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+    if (n2 === 0) {
+      return 'Error';
+    }
+    return m1 / m2;
   }
-  return 0;
-};
+
+  return m1 + m2;
+}
 
 module.exports = calculateNumber;
